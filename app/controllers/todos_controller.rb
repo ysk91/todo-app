2fc13b70b52e9b1addbaf09d57d9d02c98db0b64
sonoki-yusuke-cyberwave <sonoki.yusuke@cyberwave.jp>
2023-01-06 17:33:06 +0900

    todoを追加できるようにした

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,4 +1,14 @@
 class TodosController < ApplicationController
+  def create
+    @category = Category.find(params[:category_id])
+    @todo = @category.todos.build(todo_params)
+    if @todo.save
+      redirect_to categories_path
+    else
+      render 'categories'
+    end
+  end
+
   def destroy
     @todo = Todo.find(params[:id])
     ActiveRecord::Base.transaction do
@@ -6,5 +16,11 @@ class TodosController < ApplicationController
       @todo.update_column(:done_date, now)
     end
     redirect_to categories_url, notice: "ToDo「#{@todo.content}」を完了しました。"
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:content, :category_id)
   end
 end
