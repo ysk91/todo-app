93ed3cde6951d31614128873cd891a7546ac935a
sonoki-yusuke-cyberwave <sonoki.yusuke@cyberwave.jp>
2023-01-07 10:16:55 +0900

    日本語適用

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,9 @@ module Myapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
-
+    # 日本時間の適用
+    config.time_zone = 'Tokyo'
+    config.i18n.default_locale = :ja
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
