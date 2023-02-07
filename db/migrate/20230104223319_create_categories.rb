483b68a959ad6b21a4ac9a08eed4dfed1efd33f3
sonoki-yusuke-cyberwave <sonoki.yusuke@cyberwave.jp>
2023-02-07 09:06:45 +0900

    RSpecの導入

--- db/migrate/20230104223319_create_categories.rb
+++ db/migrate/20230104223319_create_categories.rb
@@ -1,5 +1,5 @@
 class CreateCategories < ActiveRecord::Migration[6.1]
-  def change
+  def up
     create_table :categories do |t|
       t.string :title,        null: false
       t.datetime :done_date,  null: true,   default: nil
