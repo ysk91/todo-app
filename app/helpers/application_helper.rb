22aa4a06ca46503a29d1119258f41e10a94ab49f
sonoki-yusuke-cyberwave <sonoki.yusuke@cyberwave.jp>
2023-02-08 11:21:00 +0900

    投稿を修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,27 @@
 module ApplicationHelper
+
+  def index?
+    controller.action_name == 'index'
+  end
+
+  def show?
+    controller.action_name == 'show'
+  end
+
+  def edit?
+    controller.action_name == 'edit'
+  end
+
+  def update?
+    controller.action_name == 'update'
+  end
+
+  def create?
+    %w(new create copy).index(controller.action_name)
+  end
+
+  def not_create?
+    !create?
+  end
+
 end
