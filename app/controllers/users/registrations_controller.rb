f4a3b0b8a95313b2b6489f8406858239014ecf59
sonoki-yusuke-cyberwave <sonoki.yusuke@cyberwave.jp>
2023-02-09 13:33:06 +0900

    ルーティングの設定

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,28 +1,28 @@
 # frozen_string_literal: true
 
-class User::RegistrationsController < Devise::RegistrationsController
-  before_action :configure_sign_up_params, only: [:create]
-  before_action :configure_account_update_params, only: [:update]
+class Users::RegistrationsController < Devise::RegistrationsController
+  # before_action :configure_sign_up_params, only: [:create]
+  # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
-  # def new
-  #   super
-  # end
+  def new
+    super
+  end
 
   # POST /resource
-  # def create
-  #   super
-  # end
+  def create
+    super
+  end
 
   # GET /resource/edit
-  # def edit
-  #   super
-  # end
+  def edit
+    super
+  end
 
   # PUT /resource
-  # def update
-  #   super
-  # end
+  def update
+    super
+  end
 
   # DELETE /resource
   # def destroy
@@ -38,17 +38,17 @@ class User::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  private
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
-  end
+  # def configure_sign_up_params
+  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  # end
 
   # If you have extra params to permit, append them to the sanitizer.
-  def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  # def configure_account_update_params
+  #   devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  # end
 
   # The path used after sign up.
   # def after_sign_up_path_for(resource)
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,18 @@ Rails.application.routes.draw do
     registrations:  'users/registrations',
     sessions:       'users/sessions'
   }
+  devise_scope :users do
+    get       'users/sign_up', to:      'users/registrations#new'
+    get       'users/edit', to:         'users/registrations#edit'
+    patch     'users', to:              'users/registrations#update'
+    put       'users', to:              'users/registrations#update'
+    delete    'users', to:              'users/registrations#destroy'
+    post      'users', to:              'users/registrations#create'
+
+    get       'users/sign_in', to:      'users/sessions#new'
+    post      'users/sign_in', to:      'users/sessions#create'
+    delete    'users/sign_out', to:     'users/sessions#destroy'
+  end
 
   resources :categories, only: [:index, :create, :edit, :update, :destroy] do
     resources :todos, only: [:create, :destroy]
