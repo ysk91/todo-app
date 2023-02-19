37e58e7cef409c7b4308bf9524a6b989db0740cd
sonoki-yusuke-cyberwave <sonoki.yusuke@cyberwave.jp>
2023-02-19 11:01:41 +0900

    Dockerfile変更前

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe 'Userシステムspec' do
+  let(:user) {FactoryBot.create(:user)}
+
   describe 'ユーザー新規作成' do
     it '新規作成できる' do
       visit new_user_path
@@ -14,25 +16,50 @@ RSpec.describe 'Userシステムspec' do
       }.to change { User.count }.by(1)
       expect(current_path).to eq(categories_path)
     end
+    it '新規作成できない' do
+      visit new_user_path
+      expect(current_path).to eq(new_user_path)
+      fill_in 'user_name', with: ''
+      fill_in 'user_email', with: ''
+      fill_in 'user_password', with: ''
+      fill_in 'user_password_confirmation', with: ''
+      expect{
+        find('input[name="commit"]').click
+      }.to change { User.count }.by(1)
+      expect(current_path).to eq(new_user_path)
+      expect(page).to have_content('') # フラッシュメッセージ
+      expect(page).to have_content('') # エラーメッセージ
+    end
   end
-  describe 'ユーザー作成できないときにnewページにリダイレクト、フラッシュメッセージを確認ん' do
-    # 新規ユーザー作成画面にアクセス
-    # フォーム入力
-    # ユーザーが1増える
-  end
-  describe 'ログイン' do
-    # 新規ユーザー作成画面にアクセス
-    # フォーム入力
-    # ユーザーが1増える
-  end
-  describe 'ログインできない' do
-    # 新規ユーザー作成画面にアクセス
-    # フォーム入力
-    # ユーザーが1増える
-  end
-  describe 'ログアウト' do
-    # 新規ユーザー作成画面にアクセス
-    # フォーム入力
-    # ユーザーが1増える
+  describe 'ログイン, ログアウト' do
+    it 'ログインできる' do
+      visit login_path
+      expect(current_path).to eq(login_path)
+      fill_in 'user_email', with: user.email
+      fill_in 'user_password', with: user.password
+      expect{
+        find('input[name="commit"]').click
+      }.to # current_user.name == user.name
+      expect(current_path).to eq(categories_path)
+    end
+    it 'ログインできない' do
+      visit login_path
+      expect(current_path).to eq(login_path)
+      fill_in 'user_email', with: ''
+      fill_in 'user_password', with: ''
+      expect{
+        find('input[name="commit"]').click
+      }.to # users_session.mil?
+      expect(current_path).to eq(login_path)
+      expect(page).to have_content('') # フラッシュメッセージ
+      expect(page).to have_content('') # エラーメッセージ
+    end
+    it 'ログアウトできる' do
+      before do
+        sigin_in(user)
+      end
+      visit categories_path
+
+    end
   end
 end
